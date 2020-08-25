ff38c659d7d907dcb7068c5eb970c53341a02385
fengshuo_2 <963787902@qq.com>
2020-08-25 16:38:08 +0800

    Initial commit

---
+++ Example/ABC/ABCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ABCAppDelegate.h
+//  ABC
+//
+//  Created by fengshuo1992 on 08/25/2020.
+//  Copyright (c) 2020 fengshuo1992. All rights reserved.
+//
+
+@import UIKit;
+
+@interface ABCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
