77e468e5f6f8e23dbbe43628fcba318a863f8719
fengshuo_2 <963787902@qq.com>
2020-09-02 16:44:49 +0800

    aaaaa

---
+++ ABC/Classes/ABC.m
@@ -0,0 +1,20 @@
+//
+//  ABC.m
+//  
+//
+//  Created by fengshuo on 2020/8/25.
+//
+
+#import "ABC.h"
+
+@implementation ABC
+
+- (void)test {
+    NSLog(@"这个是测试de");
+}
+
+- (void)testName:(NSString *)name {
+    NSLog(@"%@在测试",name);
+}
+
+@end
