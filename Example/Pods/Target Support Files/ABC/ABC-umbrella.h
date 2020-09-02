231089697807f2179827a1fbd681cd2099a41a98
fengshuo_2 <963787902@qq.com>
2020-09-02 23:00:36 +0800

    新增Tag对应二进制文件

---
+++ Example/Pods/Target Support Files/ABC/ABC-umbrella.h
@@ -0,0 +1,18 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "ABC.h"
+#import "FSNameObject.h"
+
+FOUNDATION_EXPORT double ABCVersionNumber;
+FOUNDATION_EXPORT const unsigned char ABCVersionString[];
+
